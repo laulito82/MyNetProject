7d6c16a071f2674d6d84431159da7e9b06f40e8d
Jeanette <jeanette.systad@dnb.no>
2021-02-22 11:38:17 +0100

    Ny klasse til prosjektet

---
+++ MyNetProject/Class123.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyNetProject
+{
+    class Class123
+    {
+        //en ny classe til det veldig viktige prosjektet
+    }
+}
