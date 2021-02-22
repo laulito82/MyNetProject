6aa8eea502902b632625bb25435e18c59594b51c
Cepeda-Vestly <raul.cepeda-vestly@dnb.no>
2021-02-22 09:37:06 +0100

    Add project files.

---
+++ MyNetProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyNetProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
