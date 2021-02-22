f90adb54ddc4e17792dcee82b141de0f4d218e6e
Raul Cepeda-Vestly <raul.cepeda-vestly@dnb.no>
2021-02-22 09:41:20 +0100

    trying out git in VS

--- MyNetProject/Program.cs
+++ MyNetProject/Program.cs
@@ -7,6 +7,7 @@ namespace MyNetProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Godbye World!");
         }
     }
 }
